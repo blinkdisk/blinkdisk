5fe40c563c2fb8068ee98ba10ed67b93fd064ca4
Paul Koeck <paul@koeck.dev>
2025-12-10 08:26:54 +0100

    perf(desktop): set vault id header without recreating xior

--- apps/desktop/src/lib/vault.ts
+++ apps/desktop/src/lib/vault.ts
@@ -1,10 +1,13 @@
 import xior from "xior";
 
+const client = xior.create({
+  baseURL: "blinkdiskapp://vault/",
+});
+
 export function vaultApi(vaultId?: string) {
-  return xior.create({
-    baseURL: `blinkdiskapp://vault/`,
-    headers: {
-      "Vault-Id": vaultId,
-    },
-  });
+  if (!client.config) client.config = {};
+  if (!client.config?.headers) client.config.headers = {};
+  client.config.headers["vault-id"] = vaultId;
+
+  return client;
 }
