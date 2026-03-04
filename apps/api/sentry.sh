cccbbc1a98ae24bf5597d541b89a5ac0fbf5cc00
Paul Koeck <paul@koeck.dev>
2026-03-04 19:10:02 +0100

    feat: upload sourcemaps in api & cloud

---
+++ apps/api/sentry.sh
@@ -0,0 +1,3 @@
+SENTRY_RELEASE=$(sentry-cli releases propose-version)
+sentry-cli releases new $SENTRY_RELEASE --org=$SENTRY_ORGANIZATION --project=$SENTRY_API_PROJECT
+sentry-cli sourcemaps upload --org=$SENTRY_ORGANIZATION --project=$SENTRY_API_PROJECT --release=$SENTRY_RELEASE dist
---
+++ apps/cloud/sentry.sh
@@ -0,0 +1,3 @@
+SENTRY_RELEASE=$(sentry-cli releases propose-version)
+sentry-cli releases new $SENTRY_RELEASE --org=$SENTRY_ORGANIZATION --project=$SENTRY_CLOUD_PROJECT
+sentry-cli sourcemaps upload --org=$SENTRY_ORGANIZATION --project=$SENTRY_CLOUD_PROJECT --release=$SENTRY_RELEASE dist
