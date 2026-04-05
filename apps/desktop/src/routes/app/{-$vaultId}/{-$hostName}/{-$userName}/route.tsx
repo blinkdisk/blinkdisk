e5820d05dd0c1db61d623089ad2fd7d86daede29
Paul Koeck <paul@koeck.dev>
2026-04-05 15:21:29 +0200

    refactor(desktop): move edit exclusion folder

--- apps/desktop/src/routes/app/{-$vaultId}/{-$hostName}/{-$userName}/route.tsx
+++ apps/desktop/src/routes/app/{-$vaultId}/{-$hostName}/{-$userName}/route.tsx
@@ -1,7 +1,7 @@
 import { CreateFolderDialog } from "@desktop/components/dialogs/create-folder";
 import { DeleteFolderDialog } from "@desktop/components/dialogs/delete-folder";
 import { DeleteVaultDialog } from "@desktop/components/dialogs/delete-vault";
-import { EditExclusionDialog } from "@desktop/components/dialogs/delete/edit-exclusion";
+import { EditExclusionDialog } from "@desktop/components/dialogs/edit-exclusion";
 import { FolderSettingsDialog } from "@desktop/components/dialogs/folder-settings";
 import { UpgradeDialog } from "@desktop/components/dialogs/upgrade";
 import { FolderDropzone } from "@desktop/components/folders/dropzone";
