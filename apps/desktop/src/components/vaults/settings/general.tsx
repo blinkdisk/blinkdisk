4e62a534e30656c702e63a76969ab57973b59585
Paul Koeck <paul@koeck.dev>
2026-04-12 12:44:45 +0200

    refactor: small improvements

--- apps/desktop/src/components/vaults/settings/general.tsx
+++ apps/desktop/src/components/vaults/settings/general.tsx
@@ -43,7 +43,7 @@ export function VaultGeneralSettings() {
           <form.Submit disabled={!isDirty}>{t("save")}</form.Submit>
         </form.AppForm>
       </form>
-      <div className="border-destructive/30 relative mt-8 flex justify-between rounded-xl border-2 border-dashed p-4">
+      <div className="mt-8 flex justify-between">
         <div>
           <p className="m-0! font-medium">{t("delete.title")}</p>
           <p className="text-muted-foreground text-xs">
