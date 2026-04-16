e9864e5eb0d5e8917f1bdaa2c6002f67f15570a9
Simon Köck <simon@koeck.dev>
2026-04-16 20:49:15 +0200

    refactor(marketing): migrate astro to v6 (#185)
    Co-authored-by: Paul Koeck <paul@koeck.dev>

--- apps/marketing/astro.config.ts
+++ apps/marketing/astro.config.ts
@@ -23,6 +23,7 @@ export default defineConfig({
   server: { port: 3000 },
   adapter: cloudflare({
     imageService: "compile",
+    prerenderEnvironment: "node",
   }),
   redirects: {
     "/pricing": {
