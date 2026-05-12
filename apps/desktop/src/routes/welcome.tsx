da2f19ac594e10236f659393f2a9c6692ee4d395
Paul Koeck <paul@koeck.dev>
2026-05-12 22:52:18 +0200

    refactor: better welcome page design

--- apps/desktop/src/routes/welcome.tsx
+++ apps/desktop/src/routes/welcome.tsx
@@ -1,7 +1,7 @@
-import { Icon } from "@blinkdisk/components/icon";
 import { LOCAL_ACCOUNT_ID } from "@blinkdisk/constants/account";
 import { useAppTranslation } from "@blinkdisk/hooks/use-app-translation";
 import { Button } from "@blinkdisk/ui/button";
+import { Logo } from "@components/logo";
 import { useAuthDialog } from "@desktop/hooks/state/use-auth-dialog";
 import { createFileRoute } from "@tanstack/react-router";
 
@@ -25,32 +25,28 @@ function RouteComponent() {
   }
 
   return (
-    <div className="bg-background background-pattern text-foreground flex min-h-screen w-screen items-center justify-center overflow-hidden px-6">
-      <main className="flex w-full max-w-3xl flex-col items-center text-center">
-        <div className="border-border bg-card shadow-foreground/10 dark:shadow-foreground/5 relative mb-12 flex size-24 items-center justify-center overflow-hidden rounded-[1.75rem] border shadow-2xl">
-          <div className="bg-linear-to-tl dark:bg-linear-to-br absolute inset-0 from-transparent dark:to-white/5"></div>
-          <Icon className="size-13 drop-shadow-red-500 drop-shadow-xl" />
-        </div>
-
-        <h1 className="text-[2.5rem] font-bold leading-tight tracking-normal md:whitespace-nowrap">
+    <div className="background-pattern text-foreground flex min-h-screen w-screen flex-col items-center justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <p className="text-muted-foreground text-lg font-semibold uppercase tracking-widest md:whitespace-nowrap">
           {t("title")}
-        </h1>
-        <p className="text-muted-foreground mt-4 max-w-md text-base leading-7 md:text-lg">
-          {t("description")}
         </p>
+        <Logo className="mt-2 h-12" />
+      </div>
+      <p className="text-muted-foreground max-w-100 mt-8 text-base leading-7 md:text-lg">
+        {t("description")}
+      </p>
 
-        <Button onClick={openAuthDialog} size="xl" className="mt-10 w-64">
-          {t("signIn")}
-        </Button>
+      <Button onClick={openAuthDialog} size="xl" className="mt-10 w-64">
+        {t("signIn")}
+      </Button>
 
-        <button
-          type="button"
-          onClick={skipAuth}
-          className="text-muted-foreground hover:text-foreground focus-visible:ring-3 focus-visible:ring-ring/50 mt-7 rounded-md px-3 py-2 text-sm outline-none transition-colors"
-        >
-          {t("skip")}
-        </button>
-      </main>
+      <button
+        type="button"
+        onClick={skipAuth}
+        className="text-muted-foreground hover:text-foreground focus-visible:ring-3 focus-visible:ring-ring/50 mt-7 rounded-md px-3 py-2 text-sm outline-none transition-colors"
+      >
+        {t("skip")}
+      </button>
     </div>
   );
 }
