111782bf0d3e746ad4a59c1ed82196a45b852e5a
Paul Koeck <paul@koeck.dev>
2026-05-30 15:09:59 +0200

    fix biome errors

--- apps/desktop/src/routes/{-$accountId}/{-$vaultId}/{-$hostName}/{-$userName}/settings.tsx
+++ apps/desktop/src/routes/{-$accountId}/{-$vaultId}/{-$hostName}/{-$userName}/settings.tsx
@@ -1,7 +1,4 @@
-import { useAppTranslation } from "@blinkdisk/hooks/use-app-translation";
 import { Accordion } from "@blinkdisk/ui/accordion";
-import { Button } from "@blinkdisk/ui/button";
-import { Skeleton } from "@blinkdisk/ui/skeleton";
 import { CompressionSettings } from "@desktop/components/policy/compression";
 import { PolicyContextProvider } from "@desktop/components/policy/context";
 import { FilesSettings } from "@desktop/components/policy/files";
@@ -10,10 +7,7 @@ import { ScheduleSettings } from "@desktop/components/policy/schedule";
 import { VaultConfigSettings } from "@desktop/components/vaults/settings/config";
 import { VaultGeneralSettings } from "@desktop/components/vaults/settings/general";
 import { VaultThrottleSettings } from "@desktop/components/vaults/settings/throttle";
-import { VaultTitlebar } from "@desktop/components/vaults/titlebar";
-import { useVault } from "@desktop/hooks/queries/use-vault";
-import { createFileRoute, Link } from "@tanstack/react-router";
-import { HomeIcon } from "lucide-react";
+import { createFileRoute } from "@tanstack/react-router";
 import { useState } from "react";
 
 export const Route = createFileRoute(
@@ -23,9 +17,6 @@ export const Route = createFileRoute(
 });
 
 function RouteComponent() {
-  const { t } = useAppTranslation("settings.vault");
-  const { data: vault } = useVault();
-
   const [open, setOpen] = useState<string[]>([]);
 
   return (
